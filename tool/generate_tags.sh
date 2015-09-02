af0ce814c1e6f80e4bfe28386c9f86f8ab265cd0
liyang <liyang@indeed.com>
2015-09-03 00:40:43 +0900

    add tags

---
+++ tool/generate_tags.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+mkdir -p tags
+rm -rf tags/*
+sed -n '/^tags:/,/---/p' all/*/_posts/*.md | grep -v 'tags:' | grep -v '\-\-\-' | sed "s/[- ]//g" |
+while read line
+do
+    mkdir -p tags/$line
+    index="tags/"$line"/index.html"
+    echo "---" > $index
+    echo "layout: tag" >> $index
+    echo "tag: $line" >> $index
+    echo "---" >> $index
+done
